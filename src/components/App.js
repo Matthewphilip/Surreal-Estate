9375e21a0916153867291b74a8211101e098ea3b
Matthew Wiggins <mattwiggins5@gmail.com>
2022-07-25 15:38:48 +0100

    initial commit - project set-up basics complete

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/app.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Surreal Estate</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
